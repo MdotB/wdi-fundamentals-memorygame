e41a153ce9f68f5736594d1cf7f3c9212e140ff5
Michael Bell <michaelbell@kevins-mbp.lan>
2018-08-18 18:26:37 -0400

    added game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,8 +1,20 @@
-console.log("Up and running!");
-
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"]; 
+var cardsInPlay =[]; //Start with empty array, where we'll add cards that are in play
+var cardOne = cards[0];
+cardsInPlay.push(cardOne); //Add first user-selected card to end of cards in play array
 
 console.log("User flipped " + cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo); //Add second user-selected card to end of cards in play array
+
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+}
